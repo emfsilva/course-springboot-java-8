83130ed2456abaa12f106350a5de3e2071278bc4
emersonfs <emerson.ferreira@unicid.edu.br>
2020-02-26 17:19:58 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -38,7 +38,7 @@ public class UserResource {
 	
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj){
-		obj = service.inser(obj);
+		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
