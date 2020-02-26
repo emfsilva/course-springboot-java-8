bc92bb4c48dce3c36b1ef71f4009d39212fe0547
emersonfs <emerson.ferreira@unicid.edu.br>
2020-02-26 11:08:32 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.services.ProductService;
 
